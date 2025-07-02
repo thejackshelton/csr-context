45bf6858a6f512fedde4c2b2a7446af7b16cd8c2
thejackshelton <me@jackshelton.com>
2025-07-02 17:11:27 -0500

    logs in render fn

--- src/components/headless/headless.tsx
+++ src/components/headless/headless.tsx
@@ -17,6 +17,7 @@ const headlessContextId = createContextId<Context>("headless-comp");
 
 export const Root = component$(() => {
 	const isChild = useSignal(false);
+	console.log("ROOT RENDER FN RAN");
 
 	const context: Context = {
 		isChild,
@@ -37,6 +38,7 @@ export const Root = component$(() => {
 
 export const Child = component$(() => {
 	const context = useContext(headlessContextId);
+	console.log("CHILD RENDER FN RAN");
 
 	useTask$(() => {
 		context.isChild.value = true;
